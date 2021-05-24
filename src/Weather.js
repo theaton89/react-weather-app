5c5e9f448d30e751f988d89194298310d31b67ca
theaton89 <taylornheaton@gmail.com>
2021-05-24 17:42:15 -0500

    Added Github link

--- src/Weather.js
+++ src/Weather.js
@@ -12,56 +12,67 @@ export default function Weather() {
     temperature: 72,
   };
   return (
-    <div className="Weather">
-      <form className="mb-3">
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Type a city..."
-              className="form-control"
-              autoComplete="off"
-            />
-          </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            />
-          </div>
-        </div>
-      </form>
-      <div className="overview">
-        <h1>{weatherData.city}</h1>
-        <ul>
-          <li>Last updated: {weatherData.date}</li>
-          <li>{weatherData.description}</li>
-        </ul>
-      </div>
-      <div className="row">
-        <div className="col-6">
-          <div className="clearfix weather-temperature">
-            <img
-              src={weatherData.imgUrl}
-              className="float-left"
-              alt={weatherData.description}
-            />
-            <div className="float-left">
-              <strong>{weatherData.temperature}</strong>
-              <span className="units">
-                <a href="/">°F</a> |<a href="/">°C</a>
-              </span>
+    <div className="attribution">
+      <div className="Weather">
+        <form className="mb-3">
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Type a city..."
+                className="form-control"
+                autoComplete="off"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
             </div>
           </div>
-        </div>
-        <div className="col-6">
+        </form>
+        <div className="overview">
+          <h1>{weatherData.city}</h1>
           <ul>
-            <li>Humidity: {weatherData.humidity}%</li>
-            <li>Wind: {weatherData.wind}mph</li>
+            <li>Last updated: {weatherData.date}</li>
+            <li>{weatherData.description}</li>
           </ul>
         </div>
+        <div className="row">
+          <div className="col-6">
+            <div className="clearfix weather-temperature">
+              <img
+                src={weatherData.imgUrl}
+                className="float-left"
+                alt={weatherData.description}
+              />
+              <div className="float-left">
+                <strong>{weatherData.temperature}</strong>
+                <span className="units">
+                  <a href="/">°F</a> |<a href="/">°C</a>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {weatherData.wind}mph</li>
+            </ul>
+          </div>
+        </div>
       </div>
+      <p>
+        This project was coded by Taylor Heaton and is{" "}
+        <a
+          href="https://github.com/theaton89/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on Github.
+        </a>
+      </p>
     </div>
   );
 }
